a4a90c33fcf0a4ea01f65cb74c60db21ab62bd88
Julien Christin <jul.christin@gmail.com>
2014-12-31 12:28:01 -0500

    Include more files to lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
+"use strict";
+
 var gulp = require("gulp"),
 	jshint = require("gulp-jshint");
 
 gulp.task("lint", function() {
-	return gulp.src(["./lib/*.js", "./test/*.js"])
+	return gulp.src(["./lib/**/*.js", "./test/**/*.js"])
 		.pipe(jshint())
 		.pipe(jshint.reporter("jshint-stylish"));
 });

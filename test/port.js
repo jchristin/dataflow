fba3b2eedca0acf2952f2639a414eadc84480caa
Julien Christin <jul.christin@gmail.com>
2014-12-31 16:01:05 -0500

    Fix typo

--- test/port.js
+++ test/port.js
@@ -6,7 +6,7 @@
 var port = require("../lib/port");
 
 describe("OutputPort", function() {
-	it("should not send data if they are noyt connected", function() {
+	it("should not send data if they are not connected", function() {
 		var outputPort = new port.OutputPort();
 		outputPort.pushData(1);
 		outputPort._readableState.length.should.be.equal(0);
